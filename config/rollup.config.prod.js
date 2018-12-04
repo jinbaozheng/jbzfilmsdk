5efd0b0264a98037b6ab40c8ae0c1efef9862fb6
cuppi <cuppi413@163.com>
2018-12-04 11:52:50 +0800

    增加rollup本地调试

---
+++ config/rollup.config.prod.js
@@ -0,0 +1,28 @@
+import typescript from 'rollup-plugin-typescript';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import { uglify } from "rollup-plugin-uglify";
+module.exports = () => {
+    return {
+        input: './src/index.ts',
+        output: {
+            name: 'jbzfilmsdk',
+            globals: {
+                moment: 'moment',
+                axios: 'axios'
+            },
+            file: './dist/index.min.js',
+            // dir: './dist',
+            format: 'umd'
+        },
+        plugins: [
+            json(),
+            resolve(),
+            commonjs(),
+            typescript({lib: ["es5", "es6", "dom"], target: "es5"}),
+            uglify()
+        ],
+        external: [ 'moment', 'axios' ]
+    }
+};
