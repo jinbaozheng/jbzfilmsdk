6116ba369b3fb9f4bbf44aed4aae28b3ecef7a79
cuppi <cuppi413@163.com>
2018-12-12 11:01:36 +0800

    增加npm cjs 格式打包文件

--- config/rollup.config.lib.js
+++ config/rollup.config.lib.js
@@ -2,7 +2,6 @@ import typescript from 'rollup-plugin-typescript';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
-import { uglify } from "rollup-plugin-uglify";
 module.exports = () => {
     return {
         input: './src/index.ts',
@@ -19,8 +18,7 @@ module.exports = () => {
             json(),
             resolve(),
             commonjs(),
-            typescript({lib: ["es5", "es6", "dom"], target: "es5"}),
-            uglify()
+            typescript({lib: ["es5", "es6", "dom"], target: "es5"})
         ],
         external: [ 'moment', 'axios' ]
     }
