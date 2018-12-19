f804061a6d86b558995007cd7503bbb15c305eb3
cuppi <cuppi413@163.com>
2018-12-19 16:44:57 +0800

    示例更新

--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,23 @@
 import unify from './spd.network.unify';
 import url from './spd.url.config'
+import {JNetworkCity, JNetworkWorker} from 'jbzfilmsdk';
+class NetworkCity extends JNetworkCity{
+    districtList(cityId){
+        return this.prefixPromise('/location/districts', this.otherParas.cityId).then(districts => {
+            return districts.map(district => {
+                return {name: district.tails.Name, id: district.id}
+            })
+        });
+    }
+    // 获取热门城市列表
+    static fetchHotCityList() {
+        return JNetworkWorker.useParas('login').POST('/location/hotcities')
+    }
+}
 
 export default {
     unify,
-    url
+    url,
+    NetworkCity,
+    C
 }
