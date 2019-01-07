3fcda9aa10a495c0b3f4a37f422e043c2fb0386e
cuppi <cuppi413@163.com>
2019-01-07 19:04:55 +0800

    增加网络请求配置类

--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,30 @@
  * Created by cuppi on 2017/4/14.
  */
 /* eslint-disable */
-import NetworkAccount from './network/JNetworkAccount';
-import NetworkCinema from './network/JNetworkCinema';
-import NetworkCity from './network/JNetworkCity';
-import NetworkFilm from './network/JNetworkFilm';
-import NetworkMine from './network/JNetworkMine';
-import NetworkOther from './network/JNetworkOther';
-import NetworkTrade from './network/JNetworkTrade';
-import NetworkConfig from './network/JNetworkConfig';
-import NetworkWorker from './network/JNetworkWorker';
+import JNetworkBanner from './network/JNetworkBanner';
+import JNetworkCinema from './network/JNetworkCinema';
+import JNetworkLocation from './network/JNetworkLocation';
+import JNetworkFilm from './network/JNetworkFilm';
+import JNetworkSearch from './network/JNetworkSearch';
+import JNetworkOther from './network/JNetworkOther';
+import JNetworkWorker from './network/JNetworkWorker';
 export {
-    NetworkAccount as JNetworkAccount,
-    NetworkCinema as JNetworkCinema,
-    NetworkCity as JNetworkCity,
-    NetworkFilm as JNetworkFilm,
-    NetworkMine as JNetworkMine,
-    NetworkOther as JNetworkOther,
-    NetworkTrade as JNetworkTrade,
-    NetworkConfig as JNetworkConfig,
-    NetworkWorker as JNetworkWorker,
+    JNetworkBanner,
+    JNetworkCinema,
+    JNetworkLocation,
+    JNetworkFilm,
+    JNetworkSearch,
+    JNetworkOther,
+    JNetworkWorker,
 };
 
 import JSDK from './util/JSDK';
-import ManagerSeat from './util/JManagerSeat';
-import ManagerTrade from './util/JManagerTrade';
+// import ManagerSeat from './util/JManagerSeat';
+// import ManagerTrade from './util/JManagerTrade';
 export {
     JSDK,
-    ManagerSeat as JManagerSeat,
-    ManagerTrade as JManagerTrade
+    // ManagerSeat as JManagerSeat,
+    // ManagerTrade as JManagerTrade
 };
 
 /* eslint-enable */
