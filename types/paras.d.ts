1affb58638218de753e1ef116de5429cbea7c5b6
wushiling <913488395@qq.com>
2019-08-08 14:46:24 +0800

    typedoc

--- types/paras.d.ts
+++ types/paras.d.ts
@@ -2,10 +2,25 @@
  * 筛选影院信息的参数声明
  */
 export declare interface CinemaFilterParas {
+    /**
+     * 影片id
+     */
     filmId: string;
+    /**
+     * 特色
+     */
     feature: string;
+    /**
+     * 区域
+     */
     region: string;
+    /**
+     * 排序
+     */
     sort: string;
+    /**
+     * 分页
+     */
     limit: string;
 }
 
@@ -16,7 +31,13 @@ export declare interface CityParas {
  *  经纬度参数声明
  */
 export declare interface CoordinateParas {
+    /**
+     * 经度
+     */
     longitude: number
+    /**
+     * 纬度
+     */
     latitude: number
 }
 
@@ -24,7 +45,16 @@ export declare interface CoordinateParas {
  * 位置信息的参数声明
  */
 export declare interface LocationParas {
+    /**
+     * 城市id
+     */
     cityId: number
+    /**
+     * 经度
+     */
     longitude: number
+    /**
+     * 纬度
+     */
     latitude: number
 }
